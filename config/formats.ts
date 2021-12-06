41f3c568690ad3229d598ac15ad370e597b86f4e
Quinn010 <quinnwilk@gmail.com>
2021-12-06 23:08:24 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -40,39 +40,6 @@ export const Formats: FormatList = [
 		banlist: [
 		'NU','OU','Porygon + Download', 'Trapinch + Arena Trap', 'Diglett + Arena Trap'
 		]
-	},
-	{
-		name: "[Gen 8] LC UU",
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3656348/">LC Metagame Discussion</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3661419/">LC Sample Teams</a>`,
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3657374/">LC Viability Rankings</a>`,
-
-		],
-
-		mod: 'gen8',
-		maxLevel: 5,
-		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Switch Priority Clause Mod','Little Cup'],
-		banlist: [
-		'NU','OU','Porygon + Download', 'Trapinch + Arena Trap', 'Diglett + Arena Trap','UU'
-	]
-
-},
-{
-	name: "[Gen 8] LC NU W.i.P",
-	threads: [
-		`&bullet; <a href="https://www.smogon.com/forums/threads/3656348/">LC Metagame Discussion</a>`,
-		`&bullet; <a href="https://www.smogon.com/forums/threads/3661419/">LC Sample Teams</a>`,
-		`&bullet; <a href="https://www.smogon.com/forums/threads/3657374/">LC Viability Rankings</a>`,
-
-	],
-
-	mod: 'gen8',
-	maxLevel: 5,
-	ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Switch Priority Clause Mod','Little Cup' ],
-	banlist: [
-	'NU','OU','Porygon + Download', 'Trapinch + Arena Trap', 'Diglett + Arena Trap','UU', 'ZU'
-]
 },
 {
 	name: "[Gen 8] Custom Gam",
